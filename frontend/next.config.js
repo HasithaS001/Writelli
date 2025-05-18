cf7f2c5fe7dd0177365b9d5c864daf7303ee1dd3
HasithaS001 <hasitha@example.com>
2025-05-18 21:35:57 +0530

    Updated CSP headers to allow new backend URL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self' data:; connect-src 'self' https://*.stripe.com https://*.supabase.co https://*.supabase.in https://www.facebook.com https://walrus-app-57iak.ondigitalocean.app; frame-src 'self';`.trim(),
+            value: `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self' data:; connect-src 'self' https://*.stripe.com https://*.supabase.co https://*.supabase.in https://www.facebook.com https://backend-yd4nj.ondigitalocean.app; frame-src 'self';`.trim(),
           },
         ],
       },
